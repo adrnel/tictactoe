738874cecc85a3ff060ceb124dfa84fd88d2007f
adrnel <anel1989@googlemail.com>
2020-02-16 15:10:53 +0000

    Add computer player

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+let board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+let count = 0;
+let winner = 0;
+let opponent = 'computer';
+let playerTurn = true;
+
 function setup() {
     createCanvas(450, 450).parent('canvas');
     document.getElementById("reset").onclick = function() {reset()};
+    document.getElementById("human").onclick = function() {opponent = 'human'};
+    document.getElementById("computer").onclick = function() {opponent = 'computer'};
 }
 
 function draw() {
@@ -20,14 +28,10 @@ function drawCircle(x = 0, y = 0) {
     circle(75 + x, 75 + y, 90);
 }
 
-let board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-let count = 0;
-let winner = 0;
 
 function mouseClicked(event) {
     if (winner === 0) {
         let isCross = count % 2 === 0;
-
         let x = -1;
         let y = -1;
         if (mouseX > 0 && mouseX <= 150) x = 0;
@@ -38,11 +42,34 @@ function mouseClicked(event) {
         if (mouseY > 300 && mouseY <= 450) y = 2;
         if (x >= 0 && y >= 0 && board[x][y] === 0) {
             count = count + 1;
+            playerTurn = !playerTurn;
             board[x][y] = isCross ? 1 : 2;
             if (isCross) drawCross(x * 150, y * 150);
             else drawCircle(x * 150, y * 150);
         }
         checkWin()
+    }
+}
+
+function computerMove() {
+    playerTurn = !playerTurn;
+    firstAvailableComputerMove();
+    count = count + 1;
+    checkWin();
+}
+
+function firstAvailableComputerMove() {
+    let isCross = count % 2 === 0;
+    let playedTurn = false;
+    for (let x = 0; x <=2; x++){
+        for (let y = 0; y <=2; y++){
+            if (board[x][y] === 0 && !playedTurn) {
+                board[x][y] = isCross ? 1 : 2;
+                playedTurn = true;
+                if (isCross) drawCross(x * 150, y * 150);
+                else drawCircle(x * 150, y * 150);
+            }
+        }
     }
 }
 
@@ -57,10 +84,11 @@ function checkWin() {
         else if (board[0][0] === i && board[1][1] === i &&  board[2][2] === i) winner = i;
         else if (board[0][2] === i && board[1][1] === i &&  board[2][0] === i) winner = i;
     }
-    if (count === 9) winner = 3;
-    if (winner === 1) document.getElementById("result").innerHTML = "X Wins";
-    if (winner === 2) document.getElementById("result").innerHTML = "O Wins";
+    if (count === 9 && winner === 0) winner = 3;
+    if (winner === 1) document.getElementById("result").innerHTML = "Xs Wins";
+    if (winner === 2) document.getElementById("result").innerHTML = "Os Wins";
     if (winner === 3) document.getElementById("result").innerHTML = "Drawn Game";
+    if (winner === 0 && count < 9 && opponent === 'computer' && !playerTurn) computerMove()
 }
 
 function reset() {
@@ -69,4 +97,8 @@ function reset() {
     document.getElementById("result").innerHTML = "";
     board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     count = 0;
+    if (opponent === 'computer') {
+        playerTurn = Math.random() >= 0.5;
+        if (!playerTurn) computerMove()
+    }
 }
